7dfc0ab79b0242e41377709cc77f395e0fc9bab2
mehmet.turan <mehmet.turan@demirorenmedya.com>
2024-01-24 23:52:40 +0300

    feature-02 / logo temaya göre değişkenlik gösterecek şekilde ayarladım.

--- layout/components/header/Header.tsx
+++ layout/components/header/Header.tsx
@@ -1,13 +1,34 @@
 import { useTheme } from "@/layout/context/Theme";
+import whiteLogo from "../../../public/L7AgencySiyah2.png";
+import darkLogo from "../../../public/L7AgencyBeyaz2.png";
+import { useRouter } from "next/navigation";
 import Container from "../container/Container";
 import "./Header.scss";
+import Nav from "./Nav";
+import Image from "next/image";
 
 const Header: React.FC<IHeaderProps> = () => {
-  const { toggleTheme } = useTheme();
+  const { theme, toggleTheme } = useTheme();
+  const logo = theme === "light" ? whiteLogo : darkLogo;
+  const navigate = useRouter();
+  const goToHome = () => {
+    navigate.push("/");
+  };
   return (
     <header>
       <Container>
-      <button onClick={toggleTheme}>Tema Değiştir</button>
+        <Image
+          src={logo}
+          alt="Lucky 7 Agency"
+          width={200}
+          height={55}
+          onClick={goToHome}
+        />
+        <Nav />
+        <div className="sub-menu">
+          <a href="/">bize ulaşın</a>
+          <button onClick={toggleTheme}>Tema Değiştir</button>
+        </div>
       </Container>
     </header>
   );
---
+++ layout/components/header/Nav.tsx
@@ -0,0 +1,16 @@
+import "./Header.scss";
+
+const Nav: React.FC<IHeaderProps> = () => {
+  return (
+    <nav>
+        <ul>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+            <li>4</li>
+            <li>5</li>
+        </ul>
+    </nav>
+  );
+};
+export default Nav;
